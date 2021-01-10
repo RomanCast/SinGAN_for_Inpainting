616c683afe84a8570412cee80dcc5c0314526675
JuliaLinhart <julia_linhart@yahoo.com>
2021-01-10 18:48:53 +0100

    Partail Convolutions: new loss, modules and training function

---
+++ SinGAN/SinGAN/partialConv2d.py
@@ -0,0 +1,155 @@
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv2d') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('Norm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+def weights_init_PC(init_type='gaussian'):
+    def init_fun(m):
+        classname = m.__class__.__name__
+        if (classname.find('Conv') == 0 or classname.find(
+                'Linear') == 0) and hasattr(m, 'weight'):
+            if init_type == 'gaussian':
+                nn.init.normal_(m.weight, 0.0, 0.02)
+            elif init_type == 'xavier':
+                nn.init.xavier_normal_(m.weight, gain=math.sqrt(2))
+            elif init_type == 'kaiming':
+                nn.init.kaiming_normal_(m.weight, a=0, mode='fan_in')
+            elif init_type == 'orthogonal':
+                nn.init.orthogonal_(m.weight, gain=math.sqrt(2))
+            elif init_type == 'default':
+                pass
+            else:
+                assert 0, "Unsupported initialization: {}".format(init_type)
+            if hasattr(m, 'bias') and m.bias is not None:
+                nn.init.constant_(m.bias, 0.0)
+
+    return init_fun
+
+
+class VGG16FeatureExtractor(nn.Module):
+    def __init__(self):
+        super().__init__()
+        vgg16 = models.vgg16(pretrained=True)
+        self.enc_1 = nn.Sequential(*vgg16.features[:5])
+        self.enc_2 = nn.Sequential(*vgg16.features[5:10])
+        self.enc_3 = nn.Sequential(*vgg16.features[10:17])
+
+        # fix the encoder
+        for i in range(3):
+            for param in getattr(self, 'enc_{:d}'.format(i + 1)).parameters():
+                param.requires_grad = False
+
+    def forward(self, image):
+        results = [image]
+        for i in range(3):
+            func = getattr(self, 'enc_{:d}'.format(i + 1))
+            results.append(func(results[-1]))
+        return results[1:]
+
+
+class PartialConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding=0, dilation=1, groups=1, bias=True):
+        super().__init__()
+        self.input_conv = nn.Conv2d(in_channels, out_channels, kernel_size,
+                                    stride, padding, dilation, groups, bias)
+        self.mask_conv = nn.Conv2d(in_channels, out_channels, kernel_size,
+                                   stride, padding, dilation, groups, False)
+        self.input_conv.apply(weights_init_PC('kaiming'))
+
+        torch.nn.init.constant_(self.mask_conv.weight, 1.0)
+
+        # mask is not updated
+        for param in self.mask_conv.parameters():
+            param.requires_grad = False
+
+    def forward(self, input, mask):
+        # http://masc.cs.gmu.edu/wiki/partialconv
+        # C(X) = W^T * X + b, C(0) = b, D(M) = 1 * M + 0 = sum(M)
+        # W^T* (M .* X) / sum(M) + b = [C(M .* X) – C(0)] / D(M) + C(0)
+
+        output = self.input_conv(input * mask)
+        if self.input_conv.bias is not None:
+            output_bias = self.input_conv.bias.view(1, -1, 1, 1).expand_as(
+                output)
+        else:
+            output_bias = torch.zeros_like(output)
+
+        with torch.no_grad():
+            output_mask = self.mask_conv(mask)
+
+        no_update_holes = output_mask == 0
+        mask_sum = output_mask.masked_fill_(no_update_holes, 1.0)
+
+        output_pre = (output - output_bias) / mask_sum + output_bias
+        output = output_pre.masked_fill_(no_update_holes, 0.0)
+
+        new_mask = torch.ones_like(output)
+        new_mask = new_mask.masked_fill_(no_update_holes, 0.0)
+
+        return output, new_mask
+
+class PartialConvBlock(nn.Module):
+    def __init__(self, in_channel, out_channel, ker_size, padd, stride):
+        super(PartialConvBlock,self).__init__()
+        self.conv = PartialConv2d(in_channel ,out_channel,kernel_size=ker_size,stride=stride,padding=padd)
+        self.bn = nn.BatchNorm2d(out_channel)
+        self.activation = nn.LeakyReLU(0.2, inplace=True)
+
+    def forward(self,input,mask):
+        x,mask_new = self.conv(input,mask)
+        x = self.bn(x)
+        x = self.activation(x)
+        return x,mask_new
+
+class PCWDiscriminator(nn.Module):
+    def __init__(self, opt):
+        super(WDiscriminator, self).__init__()
+        self.is_cuda = torch.cuda.is_available()
+        N = int(opt.nfc)
+        self.head = PartialConvBlock(opt.nc_im,N,opt.ker_size,opt.padd_size,1)
+        self.body = nn.Sequential()
+        for i in range(opt.num_layer-2):
+            N = int(opt.nfc/pow(2,(i+1)))
+            block = PartialConvBlock(max(2*N,opt.min_nfc),max(N,opt.min_nfc),opt.ker_size,opt.padd_size,1)
+            self.body.add_module('block%d'%(i+1),block)
+        self.tail = PartialConv2d(max(N,opt.min_nfc),1,kernel_size=opt.ker_size,stride=1,padding=opt.padd_size)
+
+    def forward(self,input,mask):
+        x,mask_new = self.head(input,mask)
+        x,mask_new = self.body(x,mask_new)
+        x,mask_new = self.tail(x,mask_new)
+        return x
+
+
+class PCGeneratorConcatSkip2CleanAdd(nn.Module):
+    def __init__(self, opt):
+        super(GeneratorConcatSkip2CleanAdd, self).__init__()
+        self.is_cuda = torch.cuda.is_available()
+        N = opt.nfc
+        self.head = PartialConvBlock(opt.nc_im,N,opt.ker_size,opt.padd_size,1) #GenConvTransBlock(opt.nc_z,N,opt.ker_size,opt.padd_size,opt.stride)
+        self.body = nn.Sequential()
+        for i in range(opt.num_layer-2):
+            N = int(opt.nfc/pow(2,(i+1)))
+            block = PartialConvBlock(max(2*N,opt.min_nfc),max(N,opt.min_nfc),opt.ker_size,opt.padd_size,1)
+            self.body.add_module('block%d'%(i+1),block)
+        self.tail =nn.PartialConv2d(max(N,opt.min_nfc),opt.nc_im,kernel_size=opt.ker_size,stride =1,padding=opt.padd_size)
+        self.activation = nn.Tanh()
+    def forward(self,x,y,mask):
+        x,mask_new = self.head(x,mask)
+        x,mask_new = self.body(x,mask_new)
+        x,mask_new = self.tail(x,mask_new)
+        x = self.activation(x)
+        ind = int((y.shape[2]-x.shape[2])/2)
+        y = y[:,:,ind:(y.shape[2]-ind),ind:(y.shape[3]-ind)]
+        return x+y
