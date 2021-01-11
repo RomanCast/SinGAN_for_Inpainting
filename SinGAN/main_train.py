208fe1531582d3e51f15169886fe1bb31dbaeadc
JuliaLinhart <julia_linhart@yahoo.com>
2021-01-11 13:27:21 +0100

    main train with masks

--- SinGAN/main_train.py
+++ SinGAN/main_train.py
@@ -8,13 +8,17 @@ if __name__ == '__main__':
     parser = get_arguments()
     parser.add_argument('--input_dir', help='input image dir', default='Input/Images')
     parser.add_argument('--input_name', help='input image name', required=True)
+    parser.add_argument('--ref_dir', help='input reference dir', default='Input/Inpainting')
+    parser.add_argument('--ref_name', help='reference image name', required=True)
     parser.add_argument('--mode', help='task to be done', default='train')
     opt = parser.parse_args()
     opt = functions.post_config(opt)
     Gs = []
     Zs = []
     reals = []
+    masks =[]
     NoiseAmp = []
+    mask_dir = '%s/%s_mask%s' % (opt.ref_dir,opt.ref_name[:-4],opt.ref_name[-4:])
     dir2save = functions.generate_dir2save(opt)
 
     if (os.path.exists(dir2save)):
@@ -26,5 +30,5 @@ if __name__ == '__main__':
             pass
         real = functions.read_image(opt)
         functions.adjust_scales2image(real, opt)
-        train(opt, Gs, Zs, reals, NoiseAmp)
+        train(opt, Gs, Zs, reals, masks, mask_dir, NoiseAmp)
         SinGAN_generate(Gs,Zs,reals,NoiseAmp,opt)
